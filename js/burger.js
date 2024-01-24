9cb9ca409b5f7204758177acbd30bcb73feed0b3
Guilherme Correia <guilhermeco.moraes@gmail.com>
2024-01-23 23:03:37 -0300

    update menu

--- js/burger.js
+++ js/burger.js
@@ -1,4 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const navItems = document.querySelectorAll(".nav-list a");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+// Adiciona um evento de clique para cada item da lista
+navItems.forEach(item => {
+  item.addEventListener("click", () => {
+    // Fecha o menu ao clicar em um item da lista
+    nav.classList.remove("active");
+  });
+});
